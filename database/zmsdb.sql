186fbc240508b423a66c32633def32816b26aad5
saif-smd <62687269+saif-smd@users.noreply.github.com>
2021-04-18 11:45:24 +0530

    Add files via upload

---
+++ database/zmsdb.sql
@@ -0,0 +1,243 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 17, 2021 at 09:26 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `zmsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(10) NOT NULL,
+  `AdminName` varchar(120) DEFAULT NULL,
+  `UserName` varchar(50) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(120) DEFAULT NULL,
+  `Password` varchar(120) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
+(1, 'Admin', 'admin', 8989898989, 'admin@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2019-12-30 21:38:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblanimal`
+--
+
+CREATE TABLE `tblanimal` (
+  `ID` int(10) NOT NULL,
+  `AnimalName` varchar(200) DEFAULT NULL,
+  `CageNumber` int(10) DEFAULT NULL,
+  `FeedNumber` varchar(200) DEFAULT NULL,
+  `Breed` varchar(200) DEFAULT NULL,
+  `AnimalImage` varchar(200) DEFAULT NULL,
+  `Description` mediumtext DEFAULT NULL,
+  `CreationDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tblanimal`
+--
+
+INSERT INTO `tblanimal` (`ID`, `AnimalName`, `CageNumber`, `FeedNumber`, `Breed`, `AnimalImage`, `Description`, `CreationDate`) VALUES
+(9, 'Panda', 123, 'f13', 'Ailuropoda melanoleuca', '32280d6b6794e62ea50b1d85dc98108b1618659942.jpg', ' ', '2021-04-17 11:51:30'),
+(10, 'Bear', 321, 'f34', 'Sloth Bear', '15e0c128420bf52801dd20c0d2c5cfc61618660168.jpg', ' ', '2021-04-17 11:49:52'),
+(11, 'Tiger', 345, 'f12', 'Bengal Tiger', 'a661b454fc84a42e125a349610a743161618660375.jpg', ' ', '2021-04-17 11:52:55'),
+(12, 'Giraffe', 998, 'f89', 'Marius', '2cbc8f62c38a828afbdf2a2a5b1201591618660552.jpg', ' ', '2021-04-17 11:55:52'),
+(13, 'Leopard', 343, 'd97', 'Javan Leopard', '03e81dedbdb53c236a8bd335ca13c9431618660692.jpg', ' ', '2021-04-17 11:58:12'),
+(14, 'Lion', 678, 'f98', 'African Lion', 'eeb9afd53751975645e056d85021c25d1618660948.jpg', ' ', '2021-04-17 12:02:28'),
+(15, 'Elephant', 347, 'd90', 'African savanna', '37b4c8f21df3161d12eacbfd90d43e351618661342.jpg', ' ', '2021-04-17 12:09:02');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpage`
+--
+
+CREATE TABLE `tblpage` (
+  `ID` int(10) NOT NULL,
+  `PageType` varchar(200) DEFAULT NULL,
+  `PageTitle` varchar(200) DEFAULT NULL,
+  `PageDescription` mediumtext DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tblpage`
+--
+
+INSERT INTO `tblpage` (`ID`, `PageType`, `PageTitle`, `PageDescription`, `Email`, `MobileNumber`, `UpdationDate`) VALUES
+(1, 'aboutus', ' ', '<div style=\"text-align: center;\"><span style=\"font-size: medium;\">We understand that running your business is hard work. This is a game-changer when it comes to family activity center software. Clubspeed develops and adapts our</span><span style=\"font-size: medium;\">&nbsp;solution specifically for the needs of your business; simply sit back, relax, and let us do all the heavy lifting. Then the fun will truly begin!</span></div><div style=\"text-align: center;\"><br></div>', NULL, NULL, '2021-04-17 11:37:55'),
+(2, 'contactus', '  ', '<font size=\"3\">893, 83rd street, NYC</font>', 'info@gmail.com', 1111111111, '2021-04-17 11:38:17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblticforeigner`
+--
+
+CREATE TABLE `tblticforeigner` (
+  `ID` int(10) NOT NULL,
+  `TicketID` varchar(200) DEFAULT NULL,
+  `NoAdult` int(10) DEFAULT NULL,
+  `NoChildren` int(10) DEFAULT NULL,
+  `AdultUnitprice` varchar(50) DEFAULT NULL,
+  `ChildUnitprice` varchar(50) DEFAULT NULL,
+  `PostingDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblticindian`
+--
+
+CREATE TABLE `tblticindian` (
+  `ID` int(10) NOT NULL,
+  `TicketID` varchar(100) NOT NULL,
+  `NoAdult` int(10) DEFAULT NULL,
+  `NoChildren` int(10) DEFAULT NULL,
+  `AdultUnitprice` varchar(50) DEFAULT NULL,
+  `ChildUnitprice` varchar(50) DEFAULT NULL,
+  `PostingDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbltickettype`
+--
+
+CREATE TABLE `tbltickettype` (
+  `ID` int(10) NOT NULL,
+  `TicketType` varchar(200) DEFAULT NULL,
+  `Price` varchar(50) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbltickettype`
+--
+
+INSERT INTO `tbltickettype` (`ID`, `TicketType`, `Price`, `CreationDate`) VALUES
+(1, 'Normal Adult', '350', '2019-12-30 06:31:56'),
+(2, 'Normal Child', '80', '2019-12-30 06:32:27'),
+(3, 'Foreigner Adult', '1100', '2019-12-30 06:33:16'),
+(4, 'Foreigner Child', '800', '2019-12-30 06:33:38');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblanimal`
+--
+ALTER TABLE `tblanimal`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblpage`
+--
+ALTER TABLE `tblpage`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblticforeigner`
+--
+ALTER TABLE `tblticforeigner`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `TicketID` (`TicketID`),
+  ADD KEY `TicketID_2` (`TicketID`);
+
+--
+-- Indexes for table `tblticindian`
+--
+ALTER TABLE `tblticindian`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `TicketID` (`TicketID`);
+
+--
+-- Indexes for table `tbltickettype`
+--
+ALTER TABLE `tbltickettype`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `TicketType` (`TicketType`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblanimal`
+--
+ALTER TABLE `tblanimal`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `tblpage`
+--
+ALTER TABLE `tblpage`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblticforeigner`
+--
+ALTER TABLE `tblticforeigner`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `tblticindian`
+--
+ALTER TABLE `tblticindian`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `tbltickettype`
+--
+ALTER TABLE `tbltickettype`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
